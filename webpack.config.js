b1d97d5b234126e1c235da42de801d7bd480c173
vishwasc20 <vishwas.chandrashekar@in.tesco.com>
2017-05-30 08:35:00 +0100

    Hot module reloading for CSS/LESS files - currently less and js both are minified in .js file need to separate both in future

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,36 @@ var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
 var nodeEnv = process.env.NODE_ENV || "development";
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   context: __dirname,
   devtool: debug ? "inline-source-map" : false,
-  entry: "./src/app/lib/render/client.js",
+  entry: [
+    "./src/app/assets/styles/main/index.less",
+    "./src/app/lib/render/client.js"
+  ],
   output: {
+    publicPath: '/',
     path: __dirname + "/src/",
     filename: "scripts.min.js"
+  },
+  module : {
+    loaders : [
+      {
+        test: /\.js?$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules)/,
+        loader : 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        // include: path.resolve(__dirname, 'src/app'),
+        loader: debug
+          ? "style-loader!css-loader!less-loader"
+          : ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+      }
+    ]
   },
   plugins: debug ? [
     new webpack.DefinePlugin({
@@ -21,6 +43,8 @@ module.exports = {
   ]
   :
   [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
+    new ExtractTextPlugin("style.css", { allChunks: false }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
@@ -30,20 +54,5 @@ module.exports = {
         CLIENT_SIDE: true
       }
     })
-  ],
-  module : {
-    loaders : [
-      {
-        test: /\.js?$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules)/,
-        loader : 'babel-loader'
-      },
-      {
-        test: /\.less$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: "style!css!less"
-      }
-    ]
-  }
+  ]
 };
